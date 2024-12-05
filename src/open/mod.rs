55752fe51d2886b83a732f370b62ee2eb406c0fe
reklaw28 <wturley@bu.edu>
2024-12-04 20:50:08 -0500

    making sure this is up to date

--- src/open/mod.rs
+++ src/open/mod.rs
@@ -41,79 +41,61 @@ pub fn scale(mut x: Vec<(String, i32)>, min_val: i32) -> Vec<(String, i32)> {
     return x.to_vec()}
 
 
-pub fn ad_list (x: Vec<point>) -> (Vec<(String, Vec<(String,i32)>)>){
-    let mut win_hash: HashMap::<String, Vec<String>> = HashMap::new(); // making a hash map so that i can keep track of each win a team has before constructing graph
-    let mut loss_hash: HashMap::<String, Vec<String>> = HashMap::new();
+pub fn ad_list(x: Vec<point>) -> Vec<(String, Vec<(String, i32)>)> {
+    let mut win_hash: HashMap<String, Vec<String>> = HashMap::new();
+    let mut loss_hash: HashMap<String, Vec<String>> = HashMap::new();
 
-    for i in &x{
-        win_hash.entry((*i.WIN).to_string()).or_insert(Vec::new()).push((*i.LOSS).to_string());//making the inputs append to a vec so all are there
-    }
-    //println!("{:?}",win_hash );
-    for i in &x{
-        loss_hash.entry((*i.LOSS).to_string()).or_insert(Vec::new()).push((*i.WIN).to_string());
+    // Populate win_hash and loss_hash
+    for point in &x {
+        win_hash.entry(point.WIN.clone()).or_default().push(point.LOSS.clone());
+        loss_hash.entry(point.LOSS.clone()).or_default().push(point.WIN.clone());
     }
 
-    let mut vec_of_vertex: Vec<String> = vec![]; // making a vector of all teams to use as a key 
-    for i in &win_hash{
-        if i.0 != "WIN"{// removing headers from list
-        vec_of_vertex.push(i.0.to_string());// taking only the keys
-    }
-
-    }
-    for i in &loss_hash{
-        if i.0 == "LOSS"{
-            println!("nah");
+    // Collect all unique teams
+    let mut teams: Vec<String> = win_hash.keys().cloned().collect();
+    for team in loss_hash.keys() {
+        if !teams.contains(team) {
+            teams.push(team.clone());
         }
-        else if !(vec_of_vertex.iter().any(|e| (i.0).contains(e))){
-            vec_of_vertex.push((i.0).to_string());}
-    }// making sure we have every key
+    }
 
-    // there are 1212 nodes 
+    // Build the adjacency list
+    let mut adjacency_list: Vec<(String, Vec<(String, i32)>)> = Vec::new();
+    for team in teams {
+        let mut points: Vec<(String, i32)> = Vec::new();
 
-    let mut adjacency_list: Vec<(String, Vec<(String,i32)>)> = vec![];
-    for i in vec_of_vertex{
-        let mut points: Vec<(String,i32)> = vec![];
-        if win_hash.contains_key(&i){// making sure key exists so it doesn't panic 
-        for j in &win_hash[&i]{
-            if points.iter().any(|e| j.contains(&e.0)){
-                for z in &mut points{
-                    if (*j).to_string() == z.0{
-                        z.1 +=1
-                    }
+        // Process wins
+        if let Some(opponents) = win_hash.get(&team) {
+            for opponent in opponents {
+                if let Some(point) = points.iter_mut().find(|(op, _)| op == opponent) {
+                    point.1 += 1;
+                } else {
+                    points.push((opponent.clone(), 1));
                 }
             }
-            else{
-            points.push((j.to_string(),1));
         }
-    }
 
-
-        }
-        if loss_hash.contains_key(&i){// making sure key exist so it doesnt panic
-        for j in &loss_hash[&i]{
-            if points.iter().any(|e| j.contains(&e.0)){
-                for z in &mut points{
-                    if (*j).to_string() == z.0{
-                        z.1-=1
-                    }
+        // Process losses
+        if let Some(opponents) = loss_hash.get(&team) {
+            for opponent in opponents {
+                if let Some(point) = points.iter_mut().find(|(op, _)| op == opponent) {
+                    point.1 -= 1;
+                } else {
+                    points.push((opponent.clone(), -1));
                 }
             }
-            else{
-            points.push((j.to_string(),-1));
         }
-        
-        }
+
+        adjacency_list.push((team, points));
     }
 
-    adjacency_list.push((i,points));
-    }
-    return adjacency_list
+    adjacency_list
 }
 
 #[cfg(test)]
 #[test]
 fn graph_right(){
-    let data = read_file("C:\\Users\\sirbu\\Downloads\\DS_210_lectures\\homeworks\\Final_project\\make_graph\\Final_Data.csv");
+    let data = read_file("C:\\Users\\sirbu\\Downloads\\sorted_data_real.csv");
     let mut all_point = vec![];
     for i in &data{
         let a = point{
